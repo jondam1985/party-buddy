8abd7cd83114ee8fc3514269fd3369aca57f1402
Damian Ruiz <nrt.damian@gmail.com>
2019-11-03 14:47:29 -0500

    added required fields validation

--- assets/js/invitations.js
+++ assets/js/invitations.js
@@ -1,10 +1,10 @@
 //This functions collects the data from all the fields and returns a mailto string
-var getInvitationData = function() {
-    let emailTo = $("#emails").val();
-    let host = "Invitation from: " + $("#host").val();
-    let address = "Where? : " + $("#eventAddress").val();
-    let dateTime = "When? : " + $("#eventDate").val();
-    let message =$("#message").val();
+var getInvitationData = function () {
+    let emailTo = $("#emails").val().trim();
+    let host = "Invitation from: " + $("#host").val().trim();
+    let address = "Where? : " + $("#eventAddress").val().trim();
+    let dateTime = "When? : " + $("#eventDate").val().trim();
+    let message = $("#message").val().trim();
 
     let mailto = "mailto:" + emailTo + "?subject=" + encodeMailTo(host) + "&body=" + encodeMailTo(message) + "%0D%0A%0D%0A" + encodeMailTo(address) + "%0D%0A%0D%0A" + encodeMailTo(dateTime);
 
@@ -14,16 +14,31 @@ var getInvitationData = function() {
 }
 
 //This functions encodes the string for mailto
-var encodeMailTo = function(str) {
+var encodeMailTo = function (str) {
     return encodeURIComponent(str);
 }
 
 //This function opens default mail client and populates email
-
-var sendEmail = function(mailto) {
+var sendEmail = function (mailto) {
     window.location.href = mailto;
 }
 
-$("#submit").click(function () {
-    sendEmail(getInvitationData());
+//This function shows an error message if a required field is empty
+var valError = function (elId) {
+    if ($(elId).val == "") {
+        elId.append($("<p>").text("This is a required field"));
+    }
+}
+
+
+$("#submit").click(function (e) {
+    e.preventDefault();
+    if ($("#emails").val().trim() == "" || $("#host").val().trim() == "" || $("#eventAddress").val().trim() == "" || $("#eventDate").val().trim() == "") {
+        $("p").remove();
+        $("#invitations").append($("<p>").text("Please fill all required fields."))
+    }
+    else {
+        $("p").remove();
+        sendEmail(getInvitationData());
+    }
 })
