f2702878d591fa8efa0a7e285b028edc06e9cfa2
Damian Ruiz <nrt.damian@gmail.com>
2019-11-06 01:10:18 -0500

    remove function complete

--- assets/js/renderSelections.js
+++ assets/js/renderSelections.js
@@ -1,30 +1,46 @@
 $("body").ready(function () {
 
-    $("#foodList").empty();
-    $("#drinksList").empty();
 
     //This functions renders all the selected food items in the FOOD SECTION
     $("#foodList").empty();
     let foodObj = JSON.parse(localStorage.getItem("food"));
-    let size = Object.keys(foodObj).length;
+    console.log(foodObj);
+    let size = foodObj.length;
     for (let i = 0; i < size; i++) {
         $("#foodList").append($("<div>", { "data-index": i }).html(`
-        <p>${foodObj["item" + i].title}</p>
-        <button>&times;</button>`)); // adds a "remove" button
+        <p>${foodObj[i][0]}</p>
+        <button class="foodRemover remover" data-index=${i}>&times;</button>`)); // adds a "remove" button
     }
 
     //This functions renders all the selected drink items in the DRINKS SECTION
     $("#drinksList").empty();
     let drinksObj = JSON.parse(localStorage.getItem("drinks"));
-    let length = Object.keys(drinksObj).length;
+    console.log(drinksObj);
+    let length = drinksObj.length;
     for (let i = 0; i < length; i++) {
         $("#drinksList").append($("<div>", { "data-index": i }).html(`
-        <p>${drinksObj["item" + i].title}</p>
-        <button>&times;</button>`)); // adds a "remove" button
+        <p>${drinksObj[i][0]}</p>
+        <button class="drinksRemover remover" data-index=${i}>&times;</button>`)); // adds a "remove" button
     }
+
+    $(".foodRemover").click(function () {
+        //This function removes food items from the local storage when the "remove" button is clicked
+        let foodObj = JSON.parse(localStorage.getItem("food"));
+        let toRemove = $(this).attr("data-index");
+        console.log(toRemove);
+        foodObj.splice(toRemove, 1);
+        localStorage.setItem("food", JSON.stringify(foodObj));
+        location.reload();
+    })
+
+    $(".drinksRemover").click(function () {
+        //This function removes food items from the local storage when the "remove" button is clicked
+        let drinksObj = JSON.parse(localStorage.getItem("drinks"));
+        let toRemove = $(this).attr("data-index");
+        console.log(toRemove);
+        drinksObj.splice(toRemove, 1);
+        localStorage.setItem("drinks", JSON.stringify(drinksObj));
+        location.reload();
+    })
 })
 
-$("#foodList button").click(function () {
-    let foodObj = JSON.parse(localStorage.getItem("food"));
-    let toRemove = $("#")
-})
