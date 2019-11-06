6200cd37d3b7705483ecbd14e5a4eb2651ff07b1
Damian Ruiz <nrt.damian@gmail.com>
2019-11-05 20:51:53 -0500

    add to cart updated

--- assets/js/addcart.js
+++ assets/js/addcart.js
@@ -2,9 +2,24 @@
 $(".add-to-list").click(function () {
     let title = $(".modal-title").text();
     let ingredients = [];
+    let type = $(".modal-body img").attr("src");
     $(".modal-body li").each(function (i, li) {
         ingredients.push($(li).text());
     });
+
+    console.log(type);
+    console.log(type.includes("thecocktail"));
+
+    if (localStorage.getItem("drinks") == null) {
+        let drinks = {"item0":{"title":title,"ingredients":ingredients}};
+        localStorage.setItem("drinks", JSON.stringify(drinks));
+    }
+    else {
+        let drinks = JSON.parse(localStorage.getItem("drinks"));
+        let i = Object.keys(drinks).length;
+        cart["item" + i] = {"title":title, "ingredients":ingredients};
+        localStorage.setItem("drinks", JSON.stringify(drinks));
+    }
 
     if (localStorage.getItem("cart") == null) {
         let cart = {"item0":{"title":title,"ingredients":ingredients}};
